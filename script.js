aaf866b38253bbf244e81492a66747bbacb95ce2
vladmin <vakh.vlad@gmail.com>
2018-07-12 20:42:56 +0300

    create trella rep whith index, script,readme

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// // function sum(a,b) {
+// //    return +a + +b;
+// // // };
+
+// // function factorial() {
+// //     for(){
+
+// //     }
+
+// // }
+
+
+// /*function sum(a,b) {
+//    return +a + +b;
+// };
+// */
+// //10!= 10*9*8*7*6*5*4*3*2*1
+
+
+// function factorial(n){
+//     for(var result=1; n; n-- ){
+//     result *= n ;
+//     }
+//     return result;
+// }
+// alert (factorial(3));
